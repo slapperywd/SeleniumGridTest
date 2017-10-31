788708e8f0a4ee1a10d751e76d92433a19f68f2d
Siarhei Mahlysh <maglish@mail.ru>
2017-10-31 16:25:37 +0300

    Initial commit

---
+++ SeleniumGridTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumGridTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("SeleniumGridTest")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1f811751-7f26-44a8-aaff-9a38e1fd8d25")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumGridTest/SeleniumGridTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumGridTest
+{
+    [TestClass]
+    public class SeleniumGridTest
+    {
+        [TestMethod]
+        public void ChromeMethod()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+            RemoteWebDriver driver = new RemoteWebDriver(new Uri("http://localhost:5555/wd/hub"), options.ToCapabilities());
+            driver.Navigate().GoToUrl("https://metanit.com/nosql/mongodb/1.2.php");  
+            driver.Quit();
+        }
+        [TestMethod]
+        public void FireFox()
+        {
+            FirefoxOptions options = new FirefoxOptions();
+            // options.AddArgument("--start-maximized");
+            //also works fine with http://localhost:4444/wd/hub"
+            RemoteWebDriver driver = new RemoteWebDriver(new Uri("http://localhost:5555/wd/hub"), options.ToCapabilities());
+            driver.Navigate().GoToUrl("https://metanit.com/nosql/mongodb/1.2.php");
+
+
+            driver.Quit();
+        }
+    }
+}
